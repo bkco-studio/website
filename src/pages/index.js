e2a3406d826472296de1d56549dae4987acce5fa
Robert Merrill <robgmerrill@gmail.com>
2020-01-29 15:05:07 -0800

    Add Header component to utils

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import HomeHeader from "../utils/Header"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <HomeHeader />
   </Layout>
 )
 
