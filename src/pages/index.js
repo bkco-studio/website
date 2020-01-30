6f4f1ae94f918e769f8b25771f5bf5cb0381f855
Robert Merrill <robgmerrill@gmail.com>
2020-01-30 10:12:07 -0800

    Add what we do section and Text component

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,13 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import HomeHeader from "../utils/Header"
 import OurTeam from "../components/HomePageComponents/OurTeam"
+import WhatWeDo from "../components/HomePageComponents/WhatWeDo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <HomeHeader />
+    <WhatWeDo />
     <OurTeam />
   </Layout>
 )
--- src/utils/index.js
+++ src/utils/index.js
@@ -2,4 +2,5 @@ import * as styles from "./styles"
 import HomeHeader from "./Header"
 import { Section } from "./Section"
 import { Heading } from "./Heading"
-export { styles, HomeHeader, Section, Heading }
+import { Text } from "./Text"
+export { styles, HomeHeader, Section, Heading, Text }
