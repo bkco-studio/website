3a5294075a009491c4a3e8b8da48244098860077
Robert Merrill <robgmerrill@gmail.com>
2020-02-27 10:28:09 -0800

    CaseStudyNewest component converted to grid

--- src/components/HomePageComponents/CaseStudyNewest.js
+++ src/components/HomePageComponents/CaseStudyNewest.js
@@ -2,7 +2,7 @@ import React from "react"
 
 import { StaticQuery, graphql, Link } from "gatsby"
 import styled from "styled-components"
-import { Section } from "../../utils"
+import { Section, styles } from "../../utils"
 // import CaseStudies from "../../pages/casestudies"
 
 export default () => (
@@ -33,15 +33,19 @@ export default () => (
       return (
         <Section>
           <CaseStudiesWrapper>
-            <div className="blogposts">
+            <div className="grid">
               {caseStudies.map(({ node }) => (
-                <div key={node.id}>
-                  <Link to={`/casestudy/${node.slug}`}>{node.title}</Link>
+                <span className="case-study" key={node.id}>
                   <img src={node.image.file.url} alt="" />
-                  <div>{node.body.body}</div>
-                </div>
+                  <Link
+                    className="project-title"
+                    to={`/casestudy/${node.slug}`}
+                  >
+                    {node.title}
+                  </Link>
+                  <div className="project-description">{node.body.body}</div>
+                </span>
               ))}
-              <span className="mgBtm__24" />
             </div>
           </CaseStudiesWrapper>
         </Section>
@@ -50,4 +54,40 @@ export default () => (
   />
 )
 
-const CaseStudiesWrapper = styled.div``
+const CaseStudiesWrapper = styled.div`
+  .grid {
+    display: grid;
+    grid-gap: 1em;
+    color: ${styles.colors.mainGrey};
+    grid-template-columns: 1fr 1fr;
+    .project-title {
+      color: inherit;
+      font-size: 1.5rem;
+      padding-bottom: 7px;
+      text-decoration: none;
+      cursor: pointer;
+    }
+
+    .project-description {
+      font-size: 1.12rem;
+      margin-bottom: 56px;
+    }
+
+    .case-study {
+      display: inline;
+      /* width: 48%; */
+      img {
+        max-width: 100%;
+      }
+    }
+    .case-study:first-of-type {
+      /* display: block; */
+      /* background-color: red; */
+      grid-column: 1 / 3;
+      /* width: 900px; */
+      img {
+        max-width: 100%;
+      }
+    }
+  }
+`
