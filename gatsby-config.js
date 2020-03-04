fb7430ef510b3781b34353d9595f1e7c77ac95b5
Robert Merrill <robgmerrill@gmail.com>
2020-03-04 08:44:02 -0800

    Remove fonts from gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,16 +36,16 @@ module.exports = {
     //     ],
     //   },
     // },
-    {
-      resolve: `gatsby-plugin-google-fonts`,
-      options: {
-        fonts: [
-          { family: `Noto Serif SC` },
-          { family: `Roboto`, variants: `source sans pro\:300,400,400i,700` },
-        ],
-        display: "swap",
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-google-fonts`,
+    //   options: {
+    //     fonts: [
+    //       { family: `Noto Serif SC` },
+    //       { family: `Roboto`, variants: `source sans pro\:300,400,400i,700` },
+    //     ],
+    //     display: "swap",
+    //   },
+    // },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
