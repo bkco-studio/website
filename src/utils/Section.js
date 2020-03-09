6cfe90c82f3fb4b75eca689c161c5c1862dec1a7
Robert Merrill <robgmerrill@gmail.com>
2020-03-09 09:07:57 -0700

    Section padding

--- src/utils/Section.js
+++ src/utils/Section.js
@@ -3,12 +3,13 @@ import { styles } from "../utils"
 
 export const Section = styled.section`
   padding: 2rem 0;
+  padding-left: 1.5em;
   /* padding: 42px; */
   margin: 0 auto;
   background-color: ${styles.colors.mainWhite};
   max-width: 1000px;
 
   @media (max-width: 768px) {
-    /* background-color: purple; */
+    padding-left: 1em;
   }
 `
