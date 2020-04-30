a92e0802bbd0e93e2ed3a8fac89428f953e945e9
Robert Merrill <robgmerrill@gmail.com>
2020-04-30 09:44:23 -0700

    Mobile first responsiveness on Section component

--- src/utils/Section.js
+++ src/utils/Section.js
@@ -2,13 +2,14 @@ import styled from "styled-components"
 import { styles } from "../utils"
 
 export const Section = styled.section`
-  padding: 2rem 8em;
-
   margin: 0 auto;
+  padding: 2em 1em;
+  padding-right: 1em;
+  padding-left: 1em;
   background-color: ${styles.colors.mainWhite};
 
-  @media (max-width: 768px) {
+  @media (min-width: 768px) {
+    padding: 2rem 8em;
     margin: 0 auto;
-    padding-left: 1em;
   }
 `
